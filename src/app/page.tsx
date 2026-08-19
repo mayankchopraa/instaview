972fdf0cc01e41b9094162d932ecb0b48ff6d0de
mayankchopraa <mayanktopgun@gmail.com>
2026-08-19 11:54:45 +0000

    Update InstaView branding and logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -92,7 +92,7 @@ export default function Home() {
             <img
               src="/logo-full.png"
               alt="InstaView"
-              className="h-40 w-auto object-contain"
+              className="h-20 w-auto object-contain"
             />
           </Link>
 
